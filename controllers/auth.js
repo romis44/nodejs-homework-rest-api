812be1bb677a3d542596b33edf6b607809e3ad16
romis44 <romankovalchuk44@gmail.com>
2023-05-25 12:09:57 +0300

    add token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,24 @@
+const bcrypt = require("bcrypt");
+
+const jwt = require("jsonwebtoken");
+
 const { User } = require("../models/user");
 
-const { ctrlWrapper } = require("../helpers");
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
-  const newUser = await User.create(req.body);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw new HttpError(409, "Email already is use");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
   res.status(201).json({
     email: newUser.email,
     password: newUser.password,
@@ -11,6 +26,28 @@ const register = async (req, res) => {
   });
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new HttpError(401, "Email or password is wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23" });
+
+  res.json({ token });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,10 +4,11 @@ const ctrl = require("../../controllers/auth");
 
 const { validateBody } = require("../../decorators");
 
-const { registerSchema } = require("../../schemas/userSchemas");
+const { registerSchema, loginSchema } = require("../../schemas/userSchemas");
 
 const router = express.Router();
 
 router.post("/register", validateBody(registerSchema), ctrl.register);
+router.post("/login", validateBody(loginSchema), ctrl.login);
 
 module.exports = router;
