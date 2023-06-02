01853abb4966ccb5ee463177f7041fb7be0cd010
romis44 <romankovalchuk44@gmail.com>
2023-06-02 20:01:45 +0300

    add finish changes

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -5,7 +5,7 @@ const tempDir = path.join(__dirname, "../", "temp");
 
 const multerConfig = multer.diskStorage({
   destination: tempDir,
-  filename: (req, res, cb) => {
+  filename: (req, file, cb) => {
     cb(null, file.originalname);
   },
 });
